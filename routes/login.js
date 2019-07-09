32b6fc71c17d99bf328c41e318d42125181223d1
muller <greatpage1@gmail.com>
2019-07-09 15:43:29 +0800

    token 验证

--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,13 @@
 const router = require("koa-router")()
 
-const { login }  = require("../apps/controller/login")
+const { login, sign } = require("../apps/controller/login")
 // 登录
-router.post('/login', async (ctx, next) => {
-
-    ctx.body = await login(ctx, next)
-  })
-
-
+router.post("/login/login", async (ctx, next) => {
+  ctx.body = await login(ctx, next)
+})
+// 注册
+router.post("/login/sign", async (ctx, next) => {
+  ctx.body = await sign(ctx, next)
+})
 
 module.exports = router
